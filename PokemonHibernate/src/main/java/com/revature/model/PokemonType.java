90f6c72da82e9d93dbe7b5a79d638cd6a06dd18c
Young Ha Shin <YHS@Youngs-MBP.verizon.net>
2020-11-19 11:35:48 -0500

    create table

--- PokemonHibernate/src/main/java/com/revature/model/PokemonType.java
+++ PokemonHibernate/src/main/java/com/revature/model/PokemonType.java
@@ -4,17 +4,29 @@ package com.revature.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /*
  * A bean must implement Serializable, Comparable
  * and override hashCode() and equals().
  */
+
+@Entity
+@Table(name="POKEMON_Type")
 public class PokemonType implements Serializable {
 	/**
 	 * Compatibility with Java 2.x 
 	 */
 	private static final long serialVersionUID = -2783359867056908045L;
 	
+	@Id
+	@Column(name="P_ID")
 	private int id;
+	
+	@Column(name="P_ELEMENT", nullable = false, unique = true)
 	private String element;
 	
 	public PokemonType() {}
